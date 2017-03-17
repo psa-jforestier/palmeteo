0964975b4eed4e7e1c922183fecc1a7713cbaffc
Jerome <jerome.forestier@mpsa.com>
2017-03-17 22:14:11 +0100

    improve cron process

--- client/client.php
+++ client/client.php
@@ -3,9 +3,11 @@
  ** Client side of Palmeteo.
  ** Must be launch in command line.
  **
- ** params : client.php [file_to_read] [--dry-run]
+ ** params : client.php [file_to_read] [-dr | --dry-run] [-l | --loop] [-re | --retry-on-error]
  **
- ** timeout 50 rtl_fm -f 868.26e6 -M fm -s 500k -r 75k -g 42 -A fast - | bin/rtl_868 -v -dr | php client.php -v -dr
+ ** timeout 50 rtl_fm -f 868.26e6 -M fm -s 500k -r 75k -g 42 -A fast - | bin/rtl_868 -v | php client.php -v -dr
+ **  rtl_fm -f 868.26e6 -M fm -s 500k -r 75k -g 42 -A fast - | ../bin/rtl_868 -v > temperature.dat
+ **  cat temperature.dat | php client.php -v -dr
  **/
 /**
 CREATE TABLE `report` (
@@ -32,6 +34,9 @@ if (!file_exists('config.php'))
 $CONFIG['format'] = 'csv'; // csv , json
 $CONFIG['verbose'] = false;
 $CONFIG['dryrun'] = false;
+$CONFIG['loop'] = false;
+$CONFIG['retryonerror'] = false;
+$returncode = 0;
 $i = 1;
 $file = "php://stdin";
 $verbose = false;
@@ -58,7 +63,17 @@ while($i < $argc)
 		case "-dr":
 		case "--dry-run": {
 			$CONFIG['dryrun'] = true;
-			break;
+			break;			
+		}
+		case "-l":
+		case "--loop": {
+			$CONFIG['loop'] = true;
+			break;			
+		}
+		case "-re":
+		case "--retry-on-error": {
+			$CONFIG['retryonerror'] = true;
+			break;			
 		}
 		default : {
 			$file = $param;
@@ -79,7 +94,8 @@ $fpos = 0;
 $fd = fopen($file, 'rb');
 global $NBSEND;
 $NBSEND = 0;
-while(true)
+$exit = false;
+while(!$exit)
 {
 	$fbefore = ftell($fd);
 	$s = trim(fgets($fd));
@@ -95,32 +111,53 @@ while(true)
 	}
 	else
 	{
-		echo "Unable to post data $s. Return code is $r. Wait a while and try again.\r\n";
-		sleep(4);
-		fseek($fd, $fbefore);
+		echo "Unable to post data $s. Return code is $r.\r\n";
+		$returncode = ($returncode == 0 ? 1 : 2); // =1 at 1st error, =2 on 2nd and + error
+		if ($CONFIG['retryonerror'] === true)
+		{
+			echo "Wait a while and try again.\r\n";
+			sleep(4);
+			fseek($fd, $fbefore);
+		}
 	}
 	
 	if (feof($fd))
 	{
-		$fpos = ftell($fd);
-		echo "\r\n sleep ... at pos $fpos\r\n";
-		sleep(4);
-		// reopen file
-		fclose($fd);
-		$fd = fopen($file, 'rb');
-		if (fseek($fd, $fpos) == -1)
+		if ($CONFIG['loop'])
 		{
-			echo "Unable to seek at the end of file. Truncated ? Restarting from the begining\r\n";
+			$fpos = ftell($fd);
+			echo "\r\n sleep ... at pos $fpos\r\n";
+			sleep(4);
+			// reopen file
 			fclose($fd);
 			$fd = fopen($file, 'rb');
+			if (fseek($fd, $fpos) == -1)
+			{
+				echo "Unable to seek at the end of file. Truncated ? Restarting from the begining\r\n";
+				fclose($fd);
+				$fd = fopen($file, 'rb');
+			}
+		}
+		else
+		{
+			fclose($fd);
+			$exit = true;
 		}
 	}
 }
+
+exit($returncode);
 
 function handleString($s)
 {
 	global $CONFIG;
 	$d = @split(',', $s);
+	if (count($d) < 7)
+	{
+		if ($CONFIG['verbose'] === true)
+			echo "Invalid line, skipping\r\n";
+		return 200;
+	}
 	$date = trim($d[0]);
 	$date_ts = 0 + trim($d[1]);
 	$sensorId = trim($d[2]);
--- client/client.sh
+++ client/client.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 while [ true ]
 do	
+	echo ==============
 	date
-	timeout 50 rtl_fm -f 868.26e6 -M fm -s 500k -r 75k -g 42 -A fast - | ../bin/rtl_868 > /tmp.ram/weather.dat
-	echo Datafile is :
+	echo $(date +%Y-%m-%d' '%H:%M:%S), $(date +%s), PI, $(rpi_temperature.sh | cut -c 6-9), nan, 0, \(0/0\). | tee -a /tmp.ram/weather.dat
+	timeout 40 ../bin/rtl_fm -R 30 -f 868000000 -M fm -s 500k -r 75k -g 42 -A fast | ../bin/rtl_868 | tee -a /tmp.ram/weather.dat
+	echo :: Datafile is :
 	ls -la /tmp.ram/weather.dat
-	sleep 10
+	echo :: It contains $(cat /tmp.ram/weather.dat | wc -l) line
+	echo :: Send it to the server
+	php client.php /tmp.ram/weather.dat
+	echo
+	sleep 20
+	#rm /tmp.ram/weather.dat
+	> /tmp.ram/weather.dat
 done
